fab1e4eea74ba7930777557457c9d3c383fddcdb
nw <nico.walters@techtalents.es>
2020-06-02 18:14:41 +0200

    Lazy function

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
-def my_function(maximum):
+def eager_function(maximum):
     numbers = []
     for i in range(maximum):
         numbers.append(i)
     return numbers
 
 
-total = sum(my_function(20))
+def lazy_function(maximum):
+    for i in range(maximum):
+        yield i
+
+
+
+total = sum(my_function(200000000))
 print(total)
