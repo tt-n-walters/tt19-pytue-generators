f269c799189d4198f31d6f1ab5c051ad55ed66b1
nw <nico.walters@techtalents.es>
2020-06-02 18:26:04 +0200

    Running the generator

--- script.py
+++ script.py
@@ -2,14 +2,15 @@ def eager_function(maximum):
     numbers = []
     for i in range(maximum):
         numbers.append(i)
-    return numbers
+    return numbers  # stops the function and sets the output
 
 
+# Generator
 def lazy_function(maximum):
     for i in range(maximum):
-        yield i
+        yield i    # pauses the function and sets the output
 
 
 
-total = sum(my_function(200000000))
-print(total)
+# total = sum(lazy_function(200000000))
+# print(total)
