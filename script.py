b7d37dee475ea4cc37157e415a646fd79084458a
nw <nico.walters@techtalents.es>
2020-06-02 17:58:41 +0200

    Added maximum to function arguments

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
-def my_function():
+def my_function(maximum):
     numbers = []
-    for i in range(10):
+    for i in range(maximum):
         numbers.append(i)
     return numbers
 
 
-total = sum(my_function())
+total = sum(my_function(20))
 print(total)
